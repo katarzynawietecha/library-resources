f0ec2d33f211cf6262f75f7ec305d1d767606b1a
wietecha-mrzyglod <wietecha.katarzyna@gmail.com>
2018-07-02 21:22:23 +0200

    Created: css and js files, basic js elements.

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const app = new Vue({ /*vue instance*/
+  el: "#app",
+  data: {
+    title: "Vue appliation!",
+    message: "My vue template, wooooow!"
+  }
+});
+
+const app2 = new Vue({ /*vue instance*/
+  el: "#app2",
+  data: {
+    title: "Hello",
+    message: "I'm so glad you're here!",
+    name: "Kate"
+  }
+});
